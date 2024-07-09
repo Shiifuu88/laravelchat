cfa532a1af0d520bf184f8ec5a859372d7b1e014
Shiifuu88 <bollmanndustin3@gmx.de>
2024-07-09 17:28:42 +0200

    ok

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,10 +7,3 @@ window.Alpine = Alpine;
 Alpine.start();
 
 
-window.addEventListener('beforeunload', function (e) {
-    navigator.sendBeacon('/chatrooms/leave/' + '{{ session("chat_room_id") }}');
-});
-
-window.addEventListener('unload', function (e) {
-    navigator.sendBeacon('/chatrooms/leave/' + '{{ session("chat_room_id") }}');
-});
