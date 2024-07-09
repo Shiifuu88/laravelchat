98784d06292d1fe896b1ce2c75789fb397507fa6
Shiifuu88 <bollmanndustin3@gmx.de>
2024-07-09 17:32:59 +0200

    ok

--- routes/channels.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Broadcast;
-use App\Models\ChatRoom;
-use Illuminate\Support\Facades\Auth;
-
-Broadcast::channel('chatroom.{chatRoomId}', function ($user, $chatRoomId) {
-    return Auth::check();
-});
